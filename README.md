c5447dfde51c9cf37b0c74ea2279f75debc57a6a
JonahThurston <jonah.m.thurston@gmail.com>
2025-12-02 16:55:57 -0700

    start game class

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HeartsAi
 
-This my project to try to beat my friends at the card game hearts. My vague plan so far is to make it do Monte Carlo Tree search.
+- This my project to try to beat my friends at the card game hearts. My vague plan so far is to make it do Monte Carlo Tree search.
+
+- This game follows the rules from: https://bicyclecards.com/how-to-play/hearts/
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import { HeartsGame } from "./game.js";
+
+const NUM_PLAYERS = 4; // any number 3-5
+const HUMAN_AI_ORDER = "1000"; // string where every 1 is an ai player, every 0 is a human player
+const GAME_END_POINTS = 100; //rules recomend either 50 or 100, but could do whatever
+const THINKING_TIME = 10; //time in seconds that the AI agents will be given before they must play
+
+const game = new HeartsGame(
+  NUM_PLAYERS,
+  HUMAN_AI_ORDER,
+  GAME_END_POINTS,
+  THINKING_TIME
+);
+game.run();
